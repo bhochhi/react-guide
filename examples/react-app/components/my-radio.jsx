2055f9cf58f8290e33205ce79161be80976d3151
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2016-03-09 12:43:23 -0600

    cleanup

--- examples/react-app/components/my-radio.jsx
+++ examples/react-app/components/my-radio.jsx
@@ -1,44 +1,52 @@
+var x = 200;
 var MyRadio = React.createClass({
+  getInitialState: function() {
+    return {value: 'Hello!'};
+  },
+  otherValueChange: function(event) {
+    this.setState({value: event.target.value});
+  },
   render: function() {
     return (
-      <p className="h3">My Mood</p>
-      //
-      // <p className="radio" >
-      //   <label>
-      //       <input type="radio" value="Happy" name="myRadio"/>
-      //       Happy
-      //   </label>
-      // </p>
-      //
-      // <p className="radio">
-      //       <label>
-      //         <input type="radio" value="Sad" name="myRadio" />
-      //         Sad
-      //       </label>
-      // </p>
-      //
-      // <p className="radio">
-      //     <label>
-      //       <input type="radio" value="OK" name="myRadio"/>
-      //       OK
-      //     </label>
-      // </p>
-      //
-      //   <p className = "radio" >
-      //   <label>
-      //       <input type="radio" value="Contained" name="myRadio"/>
-      //       Contained
-      //   </label>
-      //   </p>
-      //   <p className="form-group radio">
-      //       <label>
-      //         <input type="radio" value="Other" name="myRadio" />
-      //         Other
-      //       </label>
-      //       <label className="form-inline">
-      //         Specify:<input type="text" name="myRadio" / >
-      //       </label>
-      //   </p>
-);
-}
+      <div>
+        <p className="h3">My Mood</p>
+        <p className="radio">
+          <label>
+            <input type="radio" value="Happy" name="myRadio"/>
+            Happy
+          </label>
+        </p>
+
+        <p className="radio">
+          <label>
+            <input type="radio" value="Sad" name="myRadio"/>
+            Sad
+          </label>
+        </p>
+
+        <p className="radio">
+          <label>
+            <input type="radio" value="OK" name="myRadio"/>
+            OK
+          </label>
+        </p>
+
+        <p className="radio">
+          <label>
+            <input type="radio" value="Contained" name="myRadio"/>
+            Contained
+          </label>
+        </p>
+        <p className="form-group radio">
+          <label>
+            <input type="radio" value="Other" name="myRadio"/>
+            Other
+          </label>
+          <label className="form-inline">
+            Specify:<input type="text" name="myRadio"/>
+          </label>
+        </p>
+      </div>
+    );
+  }
 });
