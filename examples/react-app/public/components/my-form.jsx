60a91ce497b301c76050f363dc99ca3b5667e2ce
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2016-03-14 22:22:10 -0500

    wip

--- examples/react-app/public/components/my-form.jsx
+++ examples/react-app/public/components/my-form.jsx
@@ -1,21 +1,31 @@
 var MyRadio = require("./my-radio.jsx");
 var MyForm=React.createClass({
-  getInitialState: function() {
-    return {value: 'Hello!',
-            otherChecked:false};
-  },
-  otherValueChange: function(event) {
-    this.setState({value: event.target.value});
-  },
-  otherChecked:function(event){
-    console.log('event',event.target);
-    this.setState({otherChecked:event.target.checked})
-  },
+
   render: function() {
+    var data = [
+        {
+          value: "Happy",
+          name: "myRadio",
+          label: "Happy"
+        }, {
+          value: "Sad",
+          name: "myRadio",
+          label: "Sad"
+        }, {
+          value: "OK",
+          name: "myRadio",
+          label: "OK"
+        }, {
+          value: "Contained",
+          name: "myRadio",
+          label: "Contained"
+        }
+      ];
+    var other=true;
     return (
       <div>
         <p className="h3">My Mood</p>
-        <MyRadio/>
+        <MyRadio other={other} options={data}/>
       </div>
     );
   }
--- examples/react-app/public/components/my-radio.jsx
+++ examples/react-app/public/components/my-radio.jsx
@@ -1,40 +1,49 @@
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {value: 'Hello!',
+            otherChecked:false};
+  },
+    otherValueChange: function(event) {
+      console.log(event.target.value);
+      this.setState({value: event.target.value});
+    },
+    otherChecked:function(event){
+      console.log('event',event.target);
+      this.setState({otherChecked:event.target.checked})
+    },
+    handleMe:function(event){
+      this.setStat
+      console.log('handleMe',event.target);
+    },
   render: function() {
-    var data = [
-      {
-        value: "Happy",
-        name: "myRadio",
-        label: "Happy"
-      }, {
-        value: "Sad",
-        name: "myRadio",
-        label: "Sad"
-      }, {
-        value: "OK",
-        name: "myRadio",
-        label: "OK"
-      }, {
-        value: "Contained",
-        name: "myRadio",
-        label: "Contained"
-      }, {
-        value: "Other",
-        name: "myRadio",
-        label: "Other"
-      }
-    ];
+    console.log(this.state.otherChecked)
     return (
-      <div>
-        {_.map(data, function(radioData) {
+      <div onChange={this.handleMe}>
+
+        {_.map(this.props.options, function(radioData) {
           return (
-            <p className="radio">
+            <p className="radio" key={radioData.label}>
               <label>
                 <input  type="radio" value={radioData.value} name={radioData.name}/> {radioData.label}
               </label>
             </p>
           )
         })
-}
+      }
+      {this.props.other && (
+        <p className="radio">
+          <label>
+            <input  type="radio" value="other" name="myRadio" onChange={this.otherChecked}/> Other
+          </label>
+          {this.state.otherChecked &&
+                      <label className="form-inline">
+                       Specify:<input type="text" name="myRadio" value={this.state.value} />
+                      </label>
+
+          }
+        </p>
+      )
+    }
       </div>
     )
 
