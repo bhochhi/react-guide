d84d7140be9d8196a7d8f992e62f59b870ac3ae9
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2016-03-15 16:23:09 -0500

    fixed listeners

--- examples/react-app/public/components/my-radio.jsx
+++ examples/react-app/public/components/my-radio.jsx
@@ -1,49 +1,43 @@
 module.exports = React.createClass({
   getInitialState: function() {
-    return {value: 'Hello!',
-            otherChecked:false};
+    return {otherChecked: false};
   },
-    otherValueChange: function(event) {
-      console.log(event.target.value);
-      this.setState({value: event.target.value});
-    },
-    otherChecked:function(event){
-      console.log('event',event.target);
-      this.setState({otherChecked:event.target.checked})
-    },
-    handleMe:function(event){
-      this.setStat
-      console.log('handleMe',event.target);
-    },
+  otherCheckedHandler: function(event) {
+    console.log('other: ',event.target.checked);
+    this.setState({otherChecked: event.target.checked})
+  },
+  radioHandler: function(event){
+    console.log('Non other: ',event.target.checked);
+    this.setState({otherChecked:!event.target.checked})
+  },
   render: function() {
-    console.log(this.state.otherChecked)
+    var that = this;
     return (
-      <div onChange={this.handleMe}>
-
-        {_.map(this.props.options, function(radioData) {
+      <div>
+        { this.props.options.map(function(radioData) {
           return (
             <p className="radio" key={radioData.label}>
               <label>
-                <input  type="radio" value={radioData.value} name={radioData.name}/> {radioData.label}
+                <input type="radio" value={radioData.value} name={radioData.name} onChange={that.radioHandler} />
+                {radioData.label}
               </label>
             </p>
-          )
+          );
         })
-      }
-      {this.props.other && (
-        <p className="radio">
-          <label>
-            <input  type="radio" value="other" name="myRadio" onChange={this.otherChecked}/> Other
-          </label>
-          {this.state.otherChecked &&
-                      <label className="form-inline">
-                       Specify:<input type="text" name="myRadio" value={this.state.value} />
-                      </label>
-
-          }
-        </p>
-      )
-    }
+}
+        {this.props.other && (
+          <p className="radio">
+            <label>
+              <input type="radio" value="other" name="myRadio" onChange={this.otherCheckedHandler}/>
+              Other
+            </label>
+            {this.state.otherChecked && <label className="form-inline">
+              Specify:<input ref="Specify" type="text" name="myRadio" />
+            </label>
+}
+          </p>
+        )
+}
       </div>
     )
 
